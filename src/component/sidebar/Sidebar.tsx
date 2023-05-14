e762e2328323b8ddf5343ae5069b111c15f973e4
ianK-code <kian99564@gmail.com>
2023-05-14 14:13:50 +0300

    Added social media links

--- src/component/sidebar/Sidebar.tsx
+++ src/component/sidebar/Sidebar.tsx
@@ -1,13 +1,16 @@
 import "./sidebar.css";
+import github from "./../../assets/github.png";
+import twitter from "./../../assets/twitter.png";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <p>Github</p>
-      <p>Twitter</p>
-      <p>Linkedin</p>
-      <p>Facebook</p>
-      <p>Email</p>
+      <a href="https://github.com/Iank-code" target="_blank">
+        <img src={github} alt="github image" width={30} height={30} />
+      </a>
+      <a href="https://twitter.com/Ian_KamauKE" target="_blank">
+        <img src={twitter} alt="github image" width={30} height={30} />
+      </a>
       <hr className="vertical-hr" />
     </div>
   );
