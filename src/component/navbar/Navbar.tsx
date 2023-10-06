e1a47d575dcf16a95a0b31d7e3154dedc7288fa3
Ian Kamau <kian99564@gmail.com>
2023-10-06 12:34:45 +0300

    Changes

--- src/component/navbar/Navbar.tsx
+++ src/component/navbar/Navbar.tsx
@@ -1,26 +1,29 @@
 import "./navbar.css";
 import { NavLink } from "react-router-dom";
+import IanKamau from "./../../assets/Ian_Kamau.pdf";
 
 function Navbar() {
   // Function for downloading
-  const downloadCV = () => {
-    fetch("https://portfolio-backend-lpw2.onrender.com/documents", {
-      method: "GET",
-    })
-      .then((response) => response.blob())
-      .then((data) => {
-        console.log(data);
-
-        const url = window.URL.createObjectURL(new Blob([data]));
-        const link = document.createElement("a");
-        link.href = url;
-        link.setAttribute("download", `Ian Kamau.pdf`);
-        // 3. Append to html page
-        document.body.appendChild(link);
-        // 4. Force download
-        link.click();
-      });
-  };
+  // const downloadCV = () => {
+    // fetch("https://portfolio-backend-lpw2.onrender.com/documents", {
+    //   method: "GET",
+    // })
+    //   .then((response) => response.blob())
+    //   .then((data) => {
+    //     console.log(data);
+    //     const url = window.URL.createObjectURL(new Blob([data]));
+    //     const link = document.createElement("a");
+    //     link.href = url;
+    //     link.setAttribute("download", `Ian Kamau.pdf`);
+    //     // 3. Append to html page
+    //     document.body.appendChild(link);
+    //     // 4. Force download
+    //     link.click();
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+  // };
   return (
     <div className="navbar-container">
       <div className="name">
@@ -32,9 +35,10 @@ function Navbar() {
         {/* <span>.About()</span>
           <span onClick={() => scrollToAbout()}>.Contact()</span> */}
         <NavLink to="/all">.Projects()</NavLink>
-        <button className="nav-button" onClick={() => downloadCV()}>
+        {/* <button onClick={downloadCV}>Resume</button> */}
+        <a className="nav-button" href={IanKamau} download>
           Resume
-        </button>
+        </a>
       </div>
     </div>
   );
