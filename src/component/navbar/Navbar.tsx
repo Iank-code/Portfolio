f54fbd2dece5aeb173e80586ce9cf0726ea5b1b2
Ian Kamau <kian99564@gmail.com>
2023-10-14 22:27:39 +0300

    Making changes

--- src/component/navbar/Navbar.tsx
+++ src/component/navbar/Navbar.tsx
@@ -3,27 +3,6 @@ import { NavLink } from "react-router-dom";
 import IanKamau from "./../../assets/Ian_Kamau.pdf";
 
 function Navbar() {
-  // Function for downloading
-  // const downloadCV = () => {
-    // fetch("https://portfolio-backend-lpw2.onrender.com/documents", {
-    //   method: "GET",
-    // })
-    //   .then((response) => response.blob())
-    //   .then((data) => {
-    //     console.log(data);
-    //     const url = window.URL.createObjectURL(new Blob([data]));
-    //     const link = document.createElement("a");
-    //     link.href = url;
-    //     link.setAttribute("download", `Ian Kamau.pdf`);
-    //     // 3. Append to html page
-    //     document.body.appendChild(link);
-    //     // 4. Force download
-    //     link.click();
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
-  // };
   return (
     <div className="navbar-container">
       <div className="name">
@@ -32,10 +11,7 @@ function Navbar() {
         </NavLink>
       </div>
       <div className="links">
-        {/* <span>.About()</span>
-          <span onClick={() => scrollToAbout()}>.Contact()</span> */}
         <NavLink to="/all">.Projects()</NavLink>
-        {/* <button onClick={downloadCV}>Resume</button> */}
         <a className="nav-button" href={IanKamau} download>
           Resume
         </a>
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.js";
-import AllProjects from "./pages/AllProjects.js";
+import App from "./App.js"
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const AllProjects = lazy(() => import("./pages/AllProjects.js"));
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/all" element={<AllProjects />} />
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<>...</>}>
+              <App />
+            </Suspense>
+          }
+        />
+
+        <Route
+          path="/all"
+          element={
+            <Suspense fallback={<>...</>}>
+              <AllProjects />
+            </Suspense>
+          }
+        />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
